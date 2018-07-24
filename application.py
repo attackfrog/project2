0bd0de1545d1d0625d720e0f50ca9147dc942426
John B. McLellan <jmclellan@hbs.edu>
2018-07-24 20:11:45 +0000

    added a Wikipedia chatbot

--- application.py
+++ application.py
@@ -1,5 +1,6 @@
 import os
 from datetime import datetime
+import requests
 
 from flask import Flask, render_template
 from flask_socketio import SocketIO, emit, join_room, leave_room
@@ -8,17 +9,27 @@ app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 socketio = SocketIO(app)
 
+# Maximum number of messages per channel to keep in server memory
+MAX_SAVED_MESSAGES = 100;
+
 # Dict of channels (each being lists of message dicts)
 channels = {
     "General": [{
         "username": "attackfrog",
         "timestamp": "2018-07-23T17:16:32.082467+00:00",
-        "message": "Welcome to Chatterbox! Why don't you get the conversation started?"
+        "message": """Welcome to Chatterbox! Get the conversation started, or get information from Wikipedia on a topic by typing, 
+                      for example, '?wikipedia chatbot'"""
     }]
 }
 
 # Set of users
-users = { "0" }
+users = { "Wikipedia Bot" }
+
+# Set up User Agent so Wikipedia will accept API calls
+headers = requests.utils.default_headers()
+headers.update({
+    "User-Agent": "CS-33 Chatterbox 1.0 (attackfrog)"
+})
 
 @app.route("/")
 def index():
@@ -74,6 +85,39 @@ def message(message):
         channels[message["channel"]].append(new_message)
         emit("new message", new_message, room=message["channel"])
         
+        # send query to Wikipedia Bot if appropriate
+        if message["message"].startswith("?wikipedia "):
+            wiki_bot(message["message"], message["channel"])
+        
         # prune old messages
-        while len(channels[message["channel"]]) > 100:
+        while len(channels[message["channel"]]) > MAX_SAVED_MESSAGES:
             channels[message["channel"]].pop(0)
+
+
+def wiki_bot(query, channel):
+    # strip "?wikipedia " from the query and convert spaces to underscores
+    query = query.replace(" ", "_")[11:]
+    response = requests.get(f"https://en.wikipedia.org/w/api.php?action=query&format=json&titles={query}&prop=extracts&exintro&explaintext",
+                            headers=headers)
+    data = response.json()
+    pageid = list(data["query"]["pages"].keys())[0]
+    
+    if pageid == "-1":
+        # Page does not exist, send error message
+        message({
+            "username": "Wikipedia Bot",
+            "channel": channel,
+            "message": "That page doesn't exist on Wikipedia."
+        })
+    elif data["query"]["pages"][pageid]["extract"] == "":
+        message({
+            "username": "Wikipedia Bot",
+            "channel": channel,
+            "message": "That page exists, but the summary was blank. It may redirect to another page. Try adjusting your query."
+        })
+    else:
+        message({
+            "username": "Wikipedia Bot",
+            "channel": channel,
+            "message": data["query"]["pages"][pageid]["extract"]
+        })
